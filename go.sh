00e142eda781d2d9dfa024b37a7493aea5d596e8
James Courtois <jcourtois@users.noreply.github.com>
2018-06-07 13:54:45 -0500

    Update go.sh

--- go.sh
+++ go.sh
@@ -14,7 +14,7 @@ read -s -p 'Password: ' PASSWD
 for i in `cat list.txt`
 do
   echo "Requesting deploy for $i"
-  curl http://${GO_SERVER}:8153/go/api/pipelines/$i/schedule \
+  curl http://${GO_SERVER}/go/api/pipelines/$i/schedule \
     -u "$USERID:$PASSWD" \
     -H "Confirm: true" \
     -X POST --data "variables[DOCKER_INSTANCE]=$SLOT"
